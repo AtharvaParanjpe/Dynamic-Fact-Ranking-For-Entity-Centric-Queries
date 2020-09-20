eb451154727c77d22fe022c1a5ed1957f15862c8
AtharvaParanjpe <atharvapparanjpe@gmail.com>
2020-09-20 15:11:55 -0400

    Modification Part-1 Cleaning Utils Folder

---
+++ utils/DataPlot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from heatmap import heatmap
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+sns.set(color_codes=True, font_scale=1.2)
+
+df = pd.read_csv("../data/Complete_Data_With_Targets.csv")
+df = df.replace({'rel': {0: 'Not-relevant',1:'Relevant',2:'Very-Relevant'}})
+df = df.replace({'imp': {0: 'Not-important',1:'Important',2:'Very-Important'}})
+df['cnt'] = np.ones(len(df))
+
+
+g = df.groupby(['rel', 'imp']).count()[['cnt']].replace(np.nan, 0).reset_index()
+print(g)
+plt.rcParams["figure.figsize"] = (5,5)
+heatmap(
+    x=g['rel'], # Column to use as horizontal dimension 
+    y=g['imp'], # Column to use as vertical dimension
+    size_scale=7900, # Change this to see how it affects the plot
+    # x_order=['0', '1', '2'], # Sort order for x labels
+    # y_order=['Not important','Important','Very Important'], # Sort order for y labels
+    color=g['cnt'], # Values to map to color, here we use number of items in each bucket
+    palette=sns.cubehelix_palette(125) # We'll use black->red palette
+)
+# input()
+plt.show()
---
+++ utils/TrainTestSplit.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
+
+## filename: File to be split into Train-Test
+## train: location to store the train split
+## test: location to store the test split
+
+## HAS BEEN CALLED RECURSIVELY FOR 5-FOLD Cross valication
+
+
+filename = '../data/Complete_Data_With_Targets.csv'
+train = open("../data/CompleteData/Utility/TrainingData4.txt", "w")
+
+test = open("../data/CompleteData/Utility/TestingData4.txt", "w")
+
+
+def getPairsWithLowRanking(query,pred,obj,target,groupedDataset):
+    pairs = []
+    for q,group in groupedDataset:
+      if(query==q):
+        for i in range(len(group['pred'].values)):
+            if(group['utility'].values[i]<target):
+                train.write(query+','+pred+','+obj+','+str(target)+','+
+                query+','+group['pred'].values[i]+','+group['obj'].values[i]+','+str(group['utility'].values[i]))
+                train.write("\n")
+    
+
+df = pd.read_csv(filename)
+
+
+groupedDataset = df.groupby('query')
+
+
+queries = []
+for q,g in groupedDataset:
+  queries.append(q)
+
+queries = shuffle(queries)
+
+data_length = len(groupedDataset)
+split_length = int(0.8*data_length)
+
+queries_for_training = queries[:split_length]
+queries_for_testing = queries[split_length:]
+
+
+for query,group in groupedDataset:
+    if(query in queries_for_testing):
+        for j in range(len(group['pred'].values)):
+            test.write(query+','+group['pred'].values[j]+','+group['obj'].values[j]+','+str(group['utility'].values[j]))
+            test.write("\n")
+            
+    else:
+        for i in range(len(group['pred'].values)):
+            getPairsWithLowRanking(query,group['pred'].values[i],group['obj'].values[i],group['utility'].values[i],groupedDataset)
+
+train.close()
+test.close()
+
+
